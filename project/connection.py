00eb42374b6ecb50859b1e7a56c5621e144345e6
Saurabh72 <saurabh@proshort.ai>
2024-05-03 04:30:34 +0530

    Url updated

--- project/connection.py
+++ project/connection.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import SQLAlchemyError
+from config import DATABASE_CONFIG
 # from models import Base
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/postgres"
+DATABASE_URL = f"postgresql://{DATABASE_CONFIG['username']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}:{DATABASE_CONFIG['port']}/{DATABASE_CONFIG['database']}"
 
 engine = create_engine(DATABASE_URL)
 
